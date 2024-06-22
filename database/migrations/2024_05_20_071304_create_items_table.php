20267488b9517b732dc3289c39ebaba45dcb1f79
ywine20 <ywine381@gmail.com>
2024-06-22 11:31:44 +0630

    fix

--- database/migrations/2024_05_20_071304_create_items_table.php
+++ database/migrations/2024_05_20_071304_create_items_table.php
@@ -21,7 +21,7 @@ class CreateItemsTable extends Migration
             $table->unsignedBigInteger('main_category_id');
             $table->boolean('is_feature');
             $table->string('OE_No')->unique();
-            $table->float('price', 8, 2); // Specify precision and scale if needed
+            $table->float('price'); // Specify precision and scale if needed
             $table->timestamps();
 
             // Foreign key constraints
